fa60eeac7aa2bf77403199c6ecd68d9c12269ac6
Cat's Eye Technologies <cpressey@catseye.tc>
2014-04-30 20:55:35 +0100

    Add a sketch of what I'd hope a compiler would produce.

---
+++ eg/zeroes.c
@@ -0,0 +1,177 @@
+/* an example of what I would hope a Tamsin->C compiler to produce.
+   but this was written by hand. */
+
+/* scanner */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct scanner {
+    char *buffer;
+    int position;
+    int reset_position;
+};
+
+char scan(struct scanner *s) {
+    char c = s->buffer[s->position];
+    if (c == '\0') {
+        return '\0';
+    } else {
+        s->position++;
+        return c;
+    }
+};
+
+void unscan(struct scanner *s) {
+    s->position = s->reset_position;
+}
+
+void commit(struct scanner *s) {
+    s->reset_position = s->position;
+}
+
+int consume(struct scanner *s, char *token) {
+    char c = scan(s);
+    //fprintf(stderr, "scanned '%c'\n", c);
+    if (c == token[0]) {
+        commit(s);
+        //fprintf(stderr, "committed '%c'\n", c);
+        return 1;
+    } else {
+        unscan(s);
+        //fprintf(stderr, "rollbacked '%c'\n", c);
+        return 0;
+    }
+};
+
+
+/* terms */
+
+struct term {
+    const char *atom;
+    struct term_list *subterms;
+};
+
+struct term_list {
+    struct term *term;
+    struct term_list *next;
+};
+
+struct term *new_term(const char *atom) {
+    struct term *t;
+    t = malloc(sizeof(struct term));
+    t->atom = atom;
+    t->subterms = NULL;
+}
+
+void add_subterm(struct term *term, struct term *subterm) {
+    struct term_list *tl;
+    tl = malloc(sizeof(struct term_list));
+    tl->term = subterm;
+    tl->next = term->subterms;
+    term->subterms = tl;        
+}
+
+
+
+char fmtbuf[1000];  /* yeesh */
+
+void term_format_r(struct term *t) {
+    struct term_list *tl;
+
+    strcat(fmtbuf, t->atom);
+    if (t->subterms != NULL) {
+        strcat(fmtbuf, "(");
+        
+        for (tl = t->subterms; tl != NULL; tl = tl->next) {
+            term_format_r(tl->term);
+            if (tl->next != NULL) {
+                strcat(fmtbuf, ",");
+            }
+        }
+        
+        strcat(fmtbuf, ")");
+    }
+}
+
+char *term_format(struct term *t) {
+    strcpy(fmtbuf, "");
+    term_format_r(t);
+    return fmtbuf;
+}
+
+/* globals */
+
+struct scanner * scanner;
+
+/* result of last production */
+
+int ok;
+struct term *result;
+
+/* gen'd protos */
+
+void tamsin_main(void);
+void tamsin_zeroes(void);
+
+/* gen'd productions */
+
+void tamsin_main(void) {
+    tamsin_zeroes();
+}
+
+void tamsin_zeroes(void) {
+    struct term *E;
+
+    if (consume(scanner, "0")) {
+        tamsin_zeroes();
+        E = result;
+        if (ok) {
+            struct term *temp = new_term("zero");
+            add_subterm(temp, E);
+            result = temp;
+        }
+    } else {
+        ok = 1;
+        struct term *temp = new_term("nil");
+        result = temp;
+    }
+}
+
+/* driver */
+
+int main(int argc, char **argv) {
+    
+    scanner = malloc(sizeof(struct scanner));
+    scanner->buffer = "0000";
+    scanner->position = 0;
+    scanner->reset_position = 0;
+
+/*
+    char c;
+    int done = 0;
+    while (!done) {
+        c = scan(scanner);
+        if (c == '\0') break;
+        fprintf(stdout, "%c", c);
+    }
+    
+    fprintf(stdout, "\n");
+    exit(0);
+*/
+
+    ok = 0;
+    result = NULL;
+
+    tamsin_main();
+
+    if (ok) {
+        //fprintf(stderr, "done\n");
+        fprintf(stdout, "%s\n", term_format(result));
+        exit(0);
+    } else {
+        fprintf(stderr, "Error message\n");
+        exit(1);
+    }
+}
