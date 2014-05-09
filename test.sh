eeff71a56db8ba50ea19b8dcef5a656f6a745b02
Cat's Eye Technologies <cpressey@catseye.tc>
2014-05-09 23:09:56 +0100

    Clean up test script even more: -f files, interpreted/compiled.

--- test.sh
+++ test.sh
@@ -4,12 +4,24 @@ FILES="
     doc/Tamsin.markdown
     doc/Tested_Examples.markdown
 "
+
+if [ x$1 = 'x-f' ]; then
+    shift
+    echo "(Testing on Falderal files '$1' only)"
+    FILES=$1
+    shift
+fi
+
+MODE=compiled
+if [ x$1 = xcompiled -o x$1 = xinterpreted ]; then
+    MODE=$1
+    shift
+fi
+
 if [ x$1 = x ]; then
    $0 interpreter &&
    $0 compiler &&
-   $0 parser &&
-   $0 desugarer &&
-   $0 analyzer &&
+   $0 compiled analyzer &&
    $0 micro &&
    echo "All tests passed!"
    exit $?
@@ -17,9 +29,11 @@ fi
 
 if [ x$1 = xtamsin ]; then
    echo "Testing things written in Tamsin only."
-   $0 parser &&
-   $0 desugarer &&
-   $0 analyzer &&
+   $0 compiled scanner &&
+   $0 compiled grammar &&
+   $0 compiled parser &&
+   $0 compiled desugarer &&
+   $0 compiled analyzer &&
    $0 micro &&
    echo "All tests passed!"
    exit $?
@@ -29,58 +43,67 @@ if [ x$1 = xthorough ]; then
    echo "Testing EVERYTHING.  Will likely take more than 5 minutes."
    $0 interpreter &&
    $0 compiler &&
-   $0 grammar &&
-   $0 scanner &&
-   $0 interpretedparser &&
-   $0 parser &&
-   $0 desugarer &&
-   $0 analyzer &&
+   $0 interpreted scanner &&
+   $0 interpreted grammar &&
+   $0 interpreted parser &&
+   $0 interpreted desugarer &&
+   $0 interpreted analyzer &&
+   $0 compiled scanner &&
+   $0 compiled grammar &&
+   $0 compiled parser &&
+   $0 compiled desugarer &&
+   $0 compiled analyzer &&
    $0 micro &&
    echo "All tests passed!"
    exit $?
 fi
 
-if [ x$2 != x -a -e $2 ]; then
-    echo "(Testing on file $2 only)"
-    FILES=$2
-fi
+#MODE=compiled
+#if [ 
 
-test_compiled() {
-    SRC=$1
-    LIBS=$2
-    CMD=$3
-    BIN=$4
-
-    echo "*** Compiling $SRC (with $LIBS)"
-    echo "*** and testing it against '$CMD'..."
-    ./build.sh
-    bin/tamsin compile $LIBS $SRC > foo.c && \
-       gcc -g -Ic_src -Lc_src foo.c -o $BIN -ltamsin || exit 1
-    for EG in eg/*.tamsin; do
-        echo $EG
-        $CMD $EG > 1.txt
-        ./$BIN <$EG > 2.txt || exit 1
-        diff -ru 1.txt 2.txt > ast.diff
-        diff -ru 1.txt 2.txt || exit 1
-    done
-    echo "Passed."
-    exit 0
+ok() {
+    echo 'ok'
 }
 
-test_interpreted() {
-    SRC=$1
-    LIBS=$2
-    CMD=$3
+test_it() {
+    MODE=$1
+    SRC=$2
+    LIBS=$3
+    CMD=$4
+    BIN=$5
+    if [ x$BIN = x ]; then
+        BIN=foo
+    fi
 
-    echo "*** Interpreting $SRC (with $LIBS)"
-    echo "*** and testing it against '$CMD'..."
-    for EG in eg/*.tamsin; do
-        echo $EG
-        $CMD $EG > 1.txt
-        bin/tamsin $LIBS $SRC <$EG > 2.txt || exit 1
-        diff -ru 1.txt 2.txt > ast.diff
-        diff -ru 1.txt 2.txt || exit 1
-    done
+    if [ $MODE = "compiled" ]; then
+        echo "*** Compiling $SRC (with $LIBS)"
+        echo "*** and testing it against '$CMD'..."
+        ./build.sh
+        bin/tamsin compile $LIBS $SRC > foo.c && \
+           gcc -g -Ic_src -Lc_src foo.c -o $BIN -ltamsin || exit 1
+        for EG in eg/*.tamsin; do
+            echo $EG
+            $CMD $EG > 1.txt
+            ./$BIN <$EG > 2.txt || exit 1
+            diff -ru 1.txt 2.txt > ast.diff
+            diff -ru 1.txt 2.txt || exit 1
+        done
+    elif [ $MODE = "interpreted" ]; then
+        echo "*** Interpreting $SRC (with $LIBS)"
+        echo "*** and testing it against '$CMD'..."
+        for EG in eg/*.tamsin; do
+            echo $EG
+            $CMD $EG > 1.txt
+            bin/tamsin $LIBS $SRC <$EG > 2.txt || exit 1
+            diff -ru 1.txt 2.txt > ast.diff
+            diff -ru 1.txt 2.txt || exit 1
+        done
+        echo "Passed."
+        exit 0
+    else
+        echo "BAD MODE"
+        exit 1
+    fi
     echo "Passed."
     exit 0
 }
@@ -93,32 +116,25 @@ elif [ x$1 = xinterpreter -o x$1 = xi ]; then
     echo "*** Testing Python interpreter..."
     falderal $VERBOSE --substring-error fixture/tamsin.py.markdown $FILES
 elif [ x$1 = xgrammar ]; then
-    echo "*** Testing interpreted Tamsin-grammar-in-Tamsin..."
-    for EG in eg/*.tamsin; do
-        echo $EG
-        bin/tamsin lib/tamsin_scanner.tamsin \
-                   mains/tamsin-grammar.tamsin <$EG || exit 1
-    done
+    test_it $MODE "mains/tamsin-grammar.tamsin" \
+                  "lib/tamsin_scanner.tamsin" \
+                  "ok"
 elif [ x$1 = xscanner ]; then
-    test_interpreted "mains/scanner.tamsin" \
-                     "lib/tamsin_scanner.tamsin" \
-                     "./bin/tamsin scan"
-elif [ x$1 = xinterpretedparser ]; then
-    test_interpreted "mains/parser.tamsin" \
-                     "lib/tamsin_scanner.tamsin lib/tamsin_parser.tamsin" \
-                     "./bin/tamsin parse"
+    test_it $MODE "mains/scanner.tamsin" \
+                  "lib/tamsin_scanner.tamsin" \
+                  "./bin/tamsin scan"
 elif [ x$1 = xparser ]; then
-    test_compiled "mains/parser.tamsin" \
+    test_it $MODE "mains/parser.tamsin" \
                   "lib/tamsin_scanner.tamsin lib/tamsin_parser.tamsin" \
                   "./bin/tamsin parse" \
                   "tamsin-parser"
 elif [ x$1 = xdesugarer ]; then
-    test_compiled "mains/desugarer.tamsin" \
+    test_it $MODE "mains/desugarer.tamsin" \
                   "lib/tamsin_scanner.tamsin lib/tamsin_parser.tamsin lib/tamsin_analyzer.tamsin" \
                   "./bin/tamsin desugar" \
                   "tamsin-desugarer"
 elif [ x$1 = xanalyzer ]; then
-    test_compiled "mains/analyzer.tamsin" \
+    test_it $MODE "mains/analyzer.tamsin" \
                   "lib/tamsin_scanner.tamsin lib/tamsin_parser.tamsin lib/tamsin_analyzer.tamsin" \
                   "./bin/tamsin analyze" \
                   "tamsin-analyzer"
